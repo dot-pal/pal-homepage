fec8871b3b2eeb3b2a3f5070a1149043acda7a64
albertov19 <64150856+albertov19@users.noreply.github.com>
2024-11-14 22:00:33 +0100

    Fix edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/dot-pal/pal-homepage/',
+            'https://github.com/dot-pal/pal-homepage/blob/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
